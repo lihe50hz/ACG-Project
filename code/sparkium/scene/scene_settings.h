9119adcc1f41827b02cc7ceb179a96cafb725318
lihe50hz <lihe50hz@gmail.com>
2024-11-06 16:15:08 +0800

    finish path tracing

--- code/sparkium/scene/scene_settings.h
+++ code/sparkium/scene/scene_settings.h
@@ -10,11 +10,11 @@ struct SceneSettings {
   glm::mat4 inv_view;        // camera_to_world
   float gamma{2.2f};
   float exposure{1.0f};
-  float persistence{0.9f};
+  float persistence{1.0f};
   uint32_t accumulated_sample{0};
-  uint32_t num_sample{10};
+  uint32_t num_sample{16};
   uint32_t num_bounces{32};
-  float clamp_value{100.0f};
+  float clamp_value{1.0f};
   float total_emission_energy{0.0f};
   uint32_t num_entity{0};
   uint32_t enable_direct_lighting{1};
